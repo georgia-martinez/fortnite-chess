35929693308ee82b1c4bccb92b474f623cda8284
georgia-martinez <gcm49@case.edu>
2023-12-11 19:17:32 -0500

    Chessboard is drawn to the screen

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,31 +1,65 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	private ShapeRenderer shapeRenderer;
+	private SpriteBatch batch;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shapeRenderer = new ShapeRenderer();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(.188f, .180f, .169f, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
+
+		int ARENA_SIZE = 8;
+		float SQUARE_SIZE = 80;
+		float BOARD_SIZE = SQUARE_SIZE * 8;
+
+		float startX = (Gdx.graphics.getWidth() - BOARD_SIZE) / 2;
+		float x = startX;
+
+		float y = (Gdx.graphics.getHeight() - BOARD_SIZE) / 2;
+
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+		for (int file = 0; file < ARENA_SIZE; file++) {
+			for (int rank = 0; rank < ARENA_SIZE; rank++) {
+				boolean isLightSquare = (file + rank) % 2 == 0;
+
+				if (isLightSquare) {
+					shapeRenderer.setColor(.467f, .584f, .377f, 1);
+				} else {
+					shapeRenderer.setColor(0.922f, 0.925f, 0.816f, 1);
+				}
+
+				shapeRenderer.rect(x, y, SQUARE_SIZE, SQUARE_SIZE);
+
+				x += SQUARE_SIZE;
+			}
+
+			x = startX;
+			y += SQUARE_SIZE;
+		}
+
+		shapeRenderer.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		shapeRenderer.dispose();
 	}
 }
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -1,13 +1,21 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+		// Set screen size to full width and height of screen
+		Graphics.DisplayMode dm = Lwjgl3ApplicationConfiguration.getDisplayMode();
+		config.setWindowedMode(dm.width, dm.height);
+
+		System.out.println(dm.width);
+		System.out.println(dm.height);
+
 		config.setForegroundFPS(60);
 		config.setTitle("fortnite-chess");
 

5baf928cbcd29fe4d297c61ee28024f9c754ec00
georgia-martinez <gcm49@case.edu>
2023-12-11 19:35:51 -0500

    Replaced shapeRenderer with sprites for drawing the board

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,7 +2,9 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -11,41 +13,50 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	private ShapeRenderer shapeRenderer;
 	private SpriteBatch batch;
+	private Pixmap pixmap;
+
+	private int ARENA_SIZE = 8;
+	private final int SQUARE_SIZE = 80;
+	private final float BOARD_SIZE = SQUARE_SIZE * 8;
+
+	private Texture lightSquare;
+	private Texture darkSquare;
+
+	private Sprite[][] squares;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		shapeRenderer = new ShapeRenderer();
-	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(.188f, .180f, .169f, 1);
-		batch.begin();
-		batch.end();
+		pixmap = new Pixmap(SQUARE_SIZE, SQUARE_SIZE, Pixmap.Format.RGBA8888);
 
-		int ARENA_SIZE = 8;
-		float SQUARE_SIZE = 80;
-		float BOARD_SIZE = SQUARE_SIZE * 8;
+		pixmap.setColor(.467f, .584f, .377f, 1);
+		pixmap.fillRectangle(0, 0, SQUARE_SIZE, SQUARE_SIZE);
+
+		lightSquare = new Texture(pixmap);
+
+		pixmap.setColor(0.922f, 0.925f, 0.816f, 1);
+		pixmap.fillRectangle(0, 0, SQUARE_SIZE, SQUARE_SIZE);
+
+		darkSquare = new Texture(pixmap);
 
 		float startX = (Gdx.graphics.getWidth() - BOARD_SIZE) / 2;
 		float x = startX;
 
 		float y = (Gdx.graphics.getHeight() - BOARD_SIZE) / 2;
 
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		squares = new Sprite[ARENA_SIZE][ARENA_SIZE];
 
 		for (int file = 0; file < ARENA_SIZE; file++) {
 			for (int rank = 0; rank < ARENA_SIZE; rank++) {
 				boolean isLightSquare = (file + rank) % 2 == 0;
 
-				if (isLightSquare) {
-					shapeRenderer.setColor(.467f, .584f, .377f, 1);
-				} else {
-					shapeRenderer.setColor(0.922f, 0.925f, 0.816f, 1);
-				}
+				Sprite square = (isLightSquare) ? new Sprite(lightSquare) : new Sprite(darkSquare);
+				square.setSize(SQUARE_SIZE, SQUARE_SIZE);
+				square.setPosition(x, y);
 
-				shapeRenderer.rect(x, y, SQUARE_SIZE, SQUARE_SIZE);
+				squares[file][rank] = square;
 
 				x += SQUARE_SIZE;
 			}
@@ -53,13 +64,27 @@ public class MyGdxGame extends ApplicationAdapter {
 			x = startX;
 			y += SQUARE_SIZE;
 		}
+	}
 
-		shapeRenderer.end();
+	@Override
+	public void render () {
+		ScreenUtils.clear(.188f, .180f, .169f, 1);
+
+		batch.begin();
+
+		for (Sprite[] row : squares) {
+			for (Sprite square : row) {
+				square.draw(batch);
+			}
+		}
+
+		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		shapeRenderer.dispose();
+		pixmap.dispose();
 	}
 }
